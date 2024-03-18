ffe77590bafd9a19b64e50c3a785b859f9e26de2
ubaidalishaikh <161380272+ubaidalishaikh@users.noreply.github.com>
2024-03-19 02:01:47 +0500

    Add files via upload
    I've completed 10th Assighment 🎉 Governer Sindh IT Intiative Kamran Khan Tessori

---
+++ Q10.js
@@ -0,0 +1,4 @@
+//  My name is Ubaid Ali
+// And i am student of GIAIC
+// Future Web Developer
+console.log("Kamran Khan Tessori");
---
+++ Q10.ts
@@ -0,0 +1,5 @@
+//  My name is Ubaid Ali
+// And i am student of GIAIC
+// Future Web Developer
+
+console.log("Kamran Khan Tessori");
